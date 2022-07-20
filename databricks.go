379e8f7e42d106b5b0a02eb9a2880fbe2a7b6f77
Eric Bannatyne <eric@sigmacomputing.com>
2022-07-20 16:47:44 -0700

    Parse timestamps using timezone

--- databricks.go
+++ databricks.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"io"
 	"io/ioutil"
+	"time"
 )
 
 func init() {
@@ -18,8 +19,19 @@ type Options struct {
 	HTTPPath string
 	MaxRows  int64
 	Timeout  int
+	Loc      *time.Location
 
 	LogOut io.Writer
+}
+
+func (o *Options) Equal(o2 *Options) bool {
+	return o.Host == o2.Host &&
+		o.Port == o2.Port &&
+		o.Token == o2.Token &&
+		o.HTTPPath == o2.HTTPPath &&
+		o.MaxRows == o2.MaxRows &&
+		o.Timeout == o2.Timeout &&
+		o.Loc.String() == o2.Loc.String()
 }
 
 var (
