810e9d28ab4f8edfa0d10c27e20e8d2eb4214329
Eric Bannatyne <aldld@users.noreply.github.com>
2022-07-21 18:31:16 -0700

    Add handling for missing types in driver (#4)

--- hive/hive.go
+++ hive/hive.go
@@ -11,6 +11,7 @@ import (
 const (
 	// TimestampFormat is JDBC compliant timestamp format
 	TimestampFormat = "2006-01-02 15:04:05.999999999"
+	DateFormat      = "2006-01-02"
 )
 
 // RPCResponse respresents thrift rpc response
